b3ed6ea7957fd1ca5b3563b72a7931612e7f0c68
Andrew Whitehead <cywolf@gmail.com>
2021-04-29 15:21:15 -0700

    remove faber test
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>

--- tests/faber.rs
+++
@@ -1,9 +0,0 @@
-use aries_askar::future::block_on;
-use aries_askar::indy_compat::print_records;
-
-#[test]
-fn faber_print_records() {
-    let db = "tests/faber.agent372766/sqlite.db";
-    let key = "Faber.Agent372766";
-    block_on(print_records(db, key)).unwrap();
-}
