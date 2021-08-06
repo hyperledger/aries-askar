7140085903ab2a7c20583ddd596e1823d9059a3b
Andrew Whitehead <cywolf@gmail.com>
2021-08-06 10:02:07 -0700

    bench signature pok
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>

--- askar-bbs/benches/sign.rs
+++ askar-bbs/benches/sign.rs
@@ -1,7 +1,9 @@
 #[macro_use]
 extern crate criterion;
 
-use askar_bbs::{DynGeneratorsV1, Message, SignatureMessages};
+use askar_bbs::{
+    DynGeneratorsV1, Message, Nonce, ProverMessages, SignatureMessages, VerifierMessages,
+};
 use askar_crypto::{
     alg::bls::{BlsKeyPair, G2},
     repr::KeyGen,
@@ -40,6 +42,55 @@ fn criterion_benchmark(c: &mut Criterion) {
                 assert!(verify.verify_signature(&keypair, &sig).unwrap());
             });
         });
+
+        let nonce = Nonce::new();
+        c.bench_function(
+            &format!("create signature pok for {} messages", message_count),
+            |b| {
+                b.iter(|| {
+                    let mut prover = ProverMessages::new(&gens);
+                    let hidden_count = message_count / 2;
+                    for (index, msg) in messages.iter().enumerate() {
+                        if index < hidden_count {
+                            prover.push_hidden(*msg).unwrap();
+                        } else {
+                            prover.push_revealed(*msg).unwrap();
+                        }
+                    }
+                    let ctx = prover.prepare(&sig).unwrap();
+                    let challenge = ctx.challenge_values().create_challenge(&gens, nonce);
+                    let _proof = ctx.complete(challenge).unwrap();
+                });
+            },
+        );
+
+        let mut prover = ProverMessages::new(&gens);
+        let hidden_count = message_count / 2;
+        for (index, msg) in messages.iter().enumerate() {
+            if index < hidden_count {
+                prover.push_hidden(*msg).unwrap();
+            } else {
+                prover.push_revealed(*msg).unwrap();
+            }
+        }
+        let ctx = prover.prepare(&sig).unwrap();
+        let challenge = ctx.challenge_values().create_challenge(&gens, nonce);
+        let proof = ctx.complete(challenge).unwrap();
+        c.bench_function(
+            &format!("verify signature pok for {} messages", message_count),
+            |b| {
+                b.iter(|| {
+                    let mut verify = VerifierMessages::new(&gens);
+                    verify.push_hidden_count(hidden_count).unwrap();
+                    for index in hidden_count..messages.len() {
+                        verify.push_revealed(messages[index]).unwrap();
+                    }
+                    let challenge = proof.challenge_values().create_challenge(&gens, nonce);
+                    let check = proof.verify(&keypair, &verify, challenge).unwrap();
+                    assert!(check);
+                });
+            },
+        );
     }
 }
 
