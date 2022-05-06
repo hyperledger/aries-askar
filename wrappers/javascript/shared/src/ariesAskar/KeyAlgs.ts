175a250357eca594ab0d49873c04b06ca1b9e3b0
Berend Sliedrecht <berend@animo.id>
2022-05-06 15:32:16 +0200

    feat(js): added keyalgs
    Signed-off-by: Berend Sliedrecht <berend@animo.id>

---
+++ wrappers/javascript/shared/src/ariesAskar/KeyAlgs.ts
@@ -0,0 +1,17 @@
+export enum KeyAlgs {
+  AesA128Gcm = 'a128gcm',
+  AesA256Gcm = 'a256gcm',
+  AesA128CbcHs256 = 'a128cbchs256',
+  AesA256CbcHs512 = 'a256cbchs512',
+  AesA128Kw = 'a128kw',
+  AesA256Kw = 'a256kw',
+  Bls12381G1 = 'bls12381g1',
+  Bls12381G2 = 'bls12381g2',
+  Chacha20C20P = 'c20p',
+  Chacha20XC20P = 'xc20p',
+  Ed25519 = 'ed25519',
+  X25519 = 'x25519',
+  EcSecp256k1 = 'k256',
+  ExSecp256r1 = 'p256',
+}
+
