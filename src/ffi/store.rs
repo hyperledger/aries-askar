de6b52844cae78a55a31fdadf5f313cc5b3d7a1a
Andrew Whitehead <cywolf@gmail.com>
2022-06-02 13:01:35 -0700

    use lock_arc instead of try_lock_arc when borrowing store
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>

--- src/ffi/store.rs
+++ src/ffi/store.rs
@@ -92,14 +92,15 @@ where
     }
 
     pub async fn borrow(&self, handle: K) -> Result<TryMutexGuard<V>, Error> {
-        self.map
+        Ok(self
+            .map
             .read()
             .await
             .get(&handle)
             .ok_or_else(|| err_msg!("Invalid resource handle"))?
             .1
-            .try_lock_arc()
-            .ok_or_else(|| err_msg!(Busy, "Resource handle in use"))
+            .lock_arc()
+            .await)
     }
 
     pub async fn remove_all(&self, store: StoreHandle) -> Result<(), Error> {
