930fd3ba5041c2c4c899b2aab446aca6b6221ed6
Berend Sliedrecht <berend@animo.id>
2022-06-09 15:50:02 +0200

    fix: no error tsignore
    Signed-off-by: Berend Sliedrecht <berend@animo.id>

--- wrappers/javascript/nodejs/src/error.ts
+++ wrappers/javascript/nodejs/src/error.ts
@@ -9,9 +9,7 @@ export const handleError = () => {
   const nativeError = allocateStringBuffer()
   nativeAriesAskar.askar_get_current_error(nativeError)
 
-  // TODO
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-argument
-  const ariesAskarErrorObject: AriesAskarErrorObject = JSON.parse(nativeError.deref())
+  const ariesAskarErrorObject = JSON.parse(nativeError.deref() as string) as AriesAskarErrorObject
 
   if (ariesAskarErrorObject.code === 0) return
 
