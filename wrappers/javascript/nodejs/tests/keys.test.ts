52971369e6acb0e88ccbfc6771f15b683797c480
Berend Sliedrecht <berend@animo.id>
2022-06-16 16:23:56 +0200

    tests(js): added bls and ed25519 keygen
    Signed-off-by: Berend Sliedrecht <berend@animo.id>

--- wrappers/javascript/nodejs/tests/keys.test.ts
+++ wrappers/javascript/nodejs/tests/keys.test.ts
@@ -4,7 +4,7 @@ import { setup } from './utils'
 
 describe('keys', () => {
   beforeAll(() => setup())
-  test('aes cbc hmac', () => {
+  xtest('aes cbc hmac', () => {
     const key = Key.generate(KeyAlgs.AesA128CbcHs256)
     expect(key.algorithm).toStrictEqual(KeyAlgs.AesA128CbcHs256)
 
@@ -19,11 +19,49 @@ describe('keys', () => {
     // const dec = key.aeadDecrypt({ ciphertext: enc.buffer, nonce, aad })
   })
 
-  test('Key wrap Key', () => {
-    const key = Key.generate(KeyAlgs.AesA128CbcHs256)
-    const key2 = Key.generate(KeyAlgs.AesA128CbcHs256)
-    const nonce = key.aeadRandomNonce
-    const key3 = key.wrapKey({ other: key2, nonce })
-    console.log(key3)
+  test('Bls G2 Keygen', () => {
+    const seed = Uint8Array.from(Buffer.from('testseed000000000000000000000001'))
+    const key = Key.fromSeed({ alg: KeyAlgs.Bls12381G2, seed })
+
+    expect(key.jwkPublic).toMatchObject({
+      crv: 'BLS12381_G2',
+      kty: 'OKP',
+      x: 'lH6hIRPzjlKW6LvPm0sHqyEbGqf8ag7UWpA_GFfefwq_kzDXSHmls9Yoza_be23zEw-pSOmKI_MGR1DahBa7Jbho2BGwDNV_QmyhxMYBwTH12Ltk_GLyPD4AP6pQVgge',
+    })
+  })
+
+  test('Bls G1 Keygen', () => {
+    const seed = Uint8Array.from(Buffer.from('testseed000000000000000000000001'))
+    const key = Key.fromSeed({ alg: KeyAlgs.Bls12381G1, seed })
+
+    expect(key.jwkPublic).toMatchObject({
+      crv: 'BLS12381_G1',
+      kty: 'OKP',
+      x: 'hsjb9FSBUJXuB1fCluEcUBLeAPgIbnZGfxPKyeN3LVjQaKFWzXfNtMFAY8VL-eu-',
+    })
+  })
+
+  test('ed25519', () => {
+    const key = Key.generate(KeyAlgs.Ed25519)
+    expect(key.algorithm).toStrictEqual(KeyAlgs.Ed25519)
+    const message = Uint8Array.from(Buffer.from('test message'))
+    const signature = key.signMessage({ message })
+    expect(key.verifySignature({ message, signature })).toStrictEqual(true)
+
+    const x25519Key = key.convertkey({ alg: KeyAlgs.X25519 })
+    const x25519Key2 = Key.generate(KeyAlgs.X25519)
+
+    const kex = x25519Key.keyFromKeyExchange({ alg: KeyAlgs.Chacha20XC20P, publicKey: x25519Key2 })
+    expect(kex).toBeInstanceOf(Key)
+
+    expect(key.jwkPublic).toMatchObject({
+      kty: 'OKP',
+      crv: 'Ed25519',
+    })
+
+    expect(key.jwkSecret).toMatchObject({
+      kty: 'OKP',
+      crv: 'Ed25519',
+    })
   })
 })
