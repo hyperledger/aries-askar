915915cb42742a4434bb41184528d7a41fcaa833
Berend Sliedrecht <berend@animo.id>
2022-08-01 10:57:20 +0200

    chore(js): remove the example app
    Signed-off-by: Berend Sliedrecht <berend@animo.id>

--- wrappers/javascript/react-native-example/tests/utils/base64url.ts
+++
@@ -1,6 +0,0 @@
-import {Buffer} from 'buffer';
-
-const base64 = (str: string) => Buffer.from(str).toString('base64');
-// base64url is not available in the buffer library
-export const base64url = (str: string) =>
-  base64(str).replace(/\+/g, '_').replace(/\//g, '-').replace(/[=]+$/g, '');
