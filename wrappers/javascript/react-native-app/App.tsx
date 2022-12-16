3175181de1a6c4e3e40e5764de0fa71c3da6ac6c
blu3beri <blu3beri@proton.me>
2022-12-16 13:24:31 +0100

    added example app
    Signed-off-by: blu3beri <blu3beri@proton.me>

---
+++ wrappers/javascript/react-native-app/App.tsx
@@ -0,0 +1,21 @@
+import { ReactNativeAriesAskar } from 'aries-askar-react-native'
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Askar version: {new ReactNativeAriesAskar().version()}</Text>
+      <StatusBar style="auto" />
+    </View>
+  )
+}
