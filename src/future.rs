60012733f3ed258953c2a9ab88601d0b7c1a3a94
Andrew Whitehead <cywolf@gmail.com>
2023-01-25 14:48:45 -0800

    add terminate function to FFI to shut down worker threads
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>

--- src/future.rs
+++ src/future.rs
@@ -1,15 +1,30 @@
-use std::{future::Future, pin::Pin, time::Duration};
+use std::{
+    future::Future,
+    pin::Pin,
+    sync::Arc,
+    thread,
+    time::{Duration, Instant},
+};
 
+use arc_swap::ArcSwapOption;
 use once_cell::sync::Lazy;
 use tokio::runtime::Runtime;
 
 pub type BoxFuture<'a, T> = Pin<Box<dyn Future<Output = T> + Send + 'a>>;
 
-static RUNTIME: Lazy<Runtime> = Lazy::new(|| Runtime::new().expect("Error creating tokio runtime"));
+static RUNTIME: Lazy<ArcSwapOption<Runtime>> = Lazy::new(|| {
+    ArcSwapOption::new(Some(Arc::new(
+        Runtime::new().expect("Error creating tokio runtime"),
+    )))
+});
 
 /// Block the current thread on an async task, when not running inside the scheduler.
 pub fn block_on<R>(f: impl Future<Output = R>) -> R {
-    RUNTIME.block_on(f)
+    if let Some(rt) = RUNTIME.load().clone() {
+        rt.block_on(f)
+    } else {
+        panic!("Runtime has been shut down");
+    }
 }
 
 /// Run a blocking task without interrupting the async scheduler.
@@ -19,28 +34,64 @@ where
     T: Send + 'static,
     F: FnOnce() -> T + Send + 'static,
 {
-    RUNTIME
-        .spawn_blocking(f)
-        .await
-        .expect("Error running blocking task")
+    if let Some(rt) = RUNTIME.load().clone() {
+        rt.spawn_blocking(f)
+            .await
+            .expect("Error running blocking task")
+    } else {
+        panic!("Runtime has been shut down");
+    }
 }
 
 /// Spawn an async task into the runtime.
 #[inline]
 pub fn spawn_ok(fut: impl Future<Output = ()> + Send + 'static) {
-    RUNTIME.spawn(fut);
+    if let Some(rt) = RUNTIME.load().clone() {
+        rt.spawn(fut);
+    }
 }
 
 /// Wait until a specific duration has passed (used in tests).
 #[doc(hidden)]
 pub async fn sleep(dur: Duration) {
-    let _rt = RUNTIME.enter();
-    tokio::time::sleep(dur).await
+    if let Some(rt) = RUNTIME.load().clone() {
+        let _rt = rt.enter();
+        tokio::time::sleep(dur).await
+    }
 }
 
 /// Cancel an async task if it does not complete after a timeout (used in tests).
 #[doc(hidden)]
 pub async fn timeout<R>(dur: Duration, f: impl Future<Output = R>) -> Option<R> {
-    let _rt = RUNTIME.enter();
-    tokio::time::timeout(dur, f).await.ok()
+    if let Some(rt) = RUNTIME.load().clone() {
+        let _rt = rt.enter();
+        tokio::time::timeout(dur, f).await.ok()
+    } else {
+        None
+    }
+}
+
+/// Shut down the async runtime.
+#[doc(hidden)]
+pub fn shutdown(max_dur: Duration) {
+    let start = Instant::now();
+    if let Some(rt_swap) = Lazy::get(&RUNTIME) {
+        if let Some(mut rt) = rt_swap.swap(None) {
+            loop {
+                match Arc::try_unwrap(rt) {
+                    Ok(rt) => {
+                        rt.shutdown_timeout(max_dur.saturating_sub(start.elapsed()));
+                        break;
+                    }
+                    Err(new_rt) => {
+                        rt = new_rt;
+                        if start.elapsed() >= max_dur {
+                            break;
+                        }
+                        thread::sleep(Duration::from_millis(1));
+                    }
+                }
+            }
+        }
+    }
 }
