4e7954b92422f4e0725022c852548cf16a89d2da
Andrew Whitehead <cywolf@gmail.com>
2023-03-02 15:53:09 -0800

    wallet upgrade refactoring
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>

--- src/migration/strategy.rs
+++ src/migration/strategy.rs
@@ -1,18 +1,13 @@
 use super::{
-    IndyKey, IndySdkToAriesAskarMigration, ProfileKey, CHACHAPOLY_KEY_LEN, CHACHAPOLY_NONCE_LEN,
+    EncryptionKey, IndyKey, IndySdkToAriesAskarMigration, ProfileKey, UpdatedIndyItem,
+    CHACHAPOLY_NONCE_LEN,
 };
-use crate::crypto::generic_array::typenum::U32;
-use crate::protect::hmac_key::{HmacDerive, HmacKey};
+use crate::crypto::buffer::SecretBytes;
+use crate::crypto::encrypt::KeyAeadInPlace;
+use crate::crypto::repr::KeySecretBytes;
+use crate::protect::EntryEncryptor;
+use crate::storage::EntryTag;
 use crate::Error;
-use askar_crypto::alg::chacha20::{Chacha20Key, C20P};
-use askar_crypto::buffer::SecretBytes;
-use askar_crypto::encrypt::KeyAeadInPlace;
-use askar_crypto::kdf::KeyDerivation;
-use askar_crypto::repr::KeySecretBytes;
-use rand::RngCore;
-use sha2::Sha256;
-
-pub type IndyTag = (u8, Vec<u8>, Vec<u8>);
 
 #[derive(Default)]
 pub(crate) struct IndyItem {
@@ -20,7 +15,7 @@ pub(crate) struct IndyItem {
     typ: Vec<u8>,
     name: Vec<u8>,
     value: Option<Vec<u8>>,
-    tags: Vec<IndyTag>,
+    tags: Vec<EntryTag>,
 }
 
 // TODO: should tags_enc and tags_plain be empty in the example?
@@ -36,63 +31,10 @@ pub(crate) struct IndyRow {
     tags_plain: Option<String>,
 }
 
-#[derive(Default)]
-pub(crate) struct UpdatedIndyItem {
-    pub(crate) id: u32,
-    pub(crate) category: Vec<u8>,
-    pub(crate) name: Vec<u8>,
-    pub(crate) value: Vec<u8>,
-    pub(crate) tags: Vec<IndyTag>,
-}
-
 pub(crate) struct Strategy {}
 
 impl Strategy {
-    pub fn encrypt_merged(
-        message: &[u8],
-        key: &Chacha20Key<C20P>,
-        hmac_key: Option<&HmacKey<Sha256, U32>>,
-    ) -> Result<Vec<u8>, Error> {
-        let mut nonce = [0u8; CHACHAPOLY_NONCE_LEN as usize];
-        match hmac_key {
-            None => rand::thread_rng().fill_bytes(&mut nonce),
-            Some(hmac_key) => {
-                hmac_key
-                    .hmac_deriver(&[message])
-                    .derive_key_bytes(&mut nonce)?;
-            }
-        };
-
-        let mut ciphertext = SecretBytes::from_slice(message);
-        key.encrypt_in_place(&mut ciphertext, &nonce, &[])?;
-        let mut res = vec![];
-        res.append(&mut nonce.to_vec());
-        res.append(&mut ciphertext.into_vec());
-        Ok(res)
-    }
-
-    fn encrypt_value(
-        category: Vec<u8>,
-        name: Vec<u8>,
-        value: Vec<u8>,
-        hmac_key: &HmacKey<Sha256, U32>,
-    ) -> Result<Vec<u8>, Error> {
-        let hasher = hmac_key;
-        // length of bytes might be incorrect
-        let category_len = &category.len().to_be_bytes()[4..];
-        let name_len = &name.len().to_be_bytes()[4..];
-
-        let mut value_key = [0u8; CHACHAPOLY_KEY_LEN as usize];
-        hasher
-            .hmac_deriver(&[category_len, &category, name_len, &name])
-            .derive_key_bytes(&mut value_key)?;
-
-        let key = Chacha20Key::<C20P>::from_secret_bytes(&value_key)?;
-
-        Self::encrypt_merged(&value, &key, None)
-    }
-
-    pub fn decrypt_merged(enc_value: &[u8], key: &Chacha20Key<C20P>) -> Result<Vec<u8>, Error> {
+    pub fn decrypt_merged(enc_value: &[u8], key: &EncryptionKey) -> Result<Vec<u8>, Error> {
         let (nonce, ciphertext) = enc_value.split_at(CHACHAPOLY_NONCE_LEN.into());
 
         let mut buffer = SecretBytes::from_slice(ciphertext);
@@ -104,24 +46,25 @@ impl Strategy {
 
     pub fn decrypt_tags(
         tags: &str,
-        name_key: &Chacha20Key<C20P>,
-        value_key: Option<&Chacha20Key<C20P>>,
-    ) -> Result<Vec<(Vec<u8>, Vec<u8>)>, Error> {
-        let mut ret: Vec<(Vec<u8>, Vec<u8>)> = vec![];
+        name_key: &EncryptionKey,
+        value_key: Option<&EncryptionKey>,
+    ) -> Result<Vec<(String, String)>, Error> {
+        let mut ret = vec![];
         for tag in tags.split(',') {
-            let t = tag.split(':').collect::<Vec<&str>>();
+            let mut t = tag.split(':');
 
-            let tag_name =
-                hex::decode(t[0]).map_err(err_map!(Input, "tag is not valid hex encoded"))?;
-            let tag_value =
-                hex::decode(t[1]).map_err(err_map!(Input, "tag is not valid hex encoded"))?;
+            let tag_name = hex::decode(t.next().unwrap())
+                .map_err(err_map!(Input, "tag is not valid hex encoded"))?;
+            let tag_value = hex::decode(t.next().unwrap())
+                .map_err(err_map!(Input, "tag is not valid hex encoded"))?;
 
-            let name = Self::decrypt_merged(&tag_name, name_key)?;
-            let value = match value_key {
-                // TODO: what is tag[1] from python
+            let name = String::from_utf8(Self::decrypt_merged(&tag_name, name_key)?)
+                .map_err(err_map!(Input, "tag name is not valid utf-8"))?;
+            let value = String::from_utf8(match value_key {
                 None => tag_value,
                 Some(value_key) => Self::decrypt_merged(&tag_value, value_key)?,
-            };
+            })
+            .map_err(err_map!(Input, "tag value is not valid utf-8"))?;
             ret.push((name, value));
         }
         Ok(ret)
@@ -129,12 +72,12 @@ impl Strategy {
 
     pub fn decrypt_item(row: IndyRow, keys: &IndyKey) -> Result<IndyItem, Error> {
         let value_key = Self::decrypt_merged(&row.key, &keys.value_key)?;
-        let value_key = Chacha20Key::<C20P>::from_secret_bytes(&value_key)?;
+        let value_key = EncryptionKey::from_secret_bytes(&value_key)?;
         let value = match row.value {
             Some(ref value) => Some(Self::decrypt_merged(value, &value_key)?),
             None => None,
         };
-        let mut tags: Vec<IndyTag> = vec![];
+        let mut tags: Vec<EntryTag> = vec![];
 
         let resp = match row.tags_enc {
             None => vec![],
@@ -144,16 +87,16 @@ impl Strategy {
                 Some(&keys.tag_value_key),
             )?,
         };
-        for (k, v) in resp {
-            tags.push((0, k, v));
+        for (name, value) in resp {
+            tags.push(EntryTag::Encrypted(name, value));
         }
 
         let resp_plain = match row.tags_plain {
             None => vec![],
             Some(tags_plain) => Self::decrypt_tags(tags_plain.as_str(), &keys.tag_name_key, None)?,
         };
-        for (k, v) in resp_plain {
-            tags.push((1, k, v));
+        for (name, value) in resp_plain {
+            tags.push(EntryTag::Plaintext(name, value));
         }
 
         let indy_item = IndyItem {
@@ -168,36 +111,17 @@ impl Strategy {
     }
 
     pub fn update_item(item: IndyItem, key: &ProfileKey) -> Result<UpdatedIndyItem, Error> {
-        let mut tags: Vec<IndyTag> = vec![];
-
-        for (plain, mut k, mut v) in item.tags {
-            if plain != 0 {
-                v = Self::encrypt_merged(&v, &key.tag_value_key, Some(&key.tags_hmac_key))?;
-            }
-            k = Self::encrypt_merged(&k, &key.tag_name_key, Some(&key.tags_hmac_key))?;
-            tags.push((plain, k, v));
-        }
+        let value = match item.value {
+            Some(v) => key.encrypt_entry_value(&item.typ, &item.name, v.into())?,
+            None => Default::default(),
+        };
 
         let updated_indy_item = UpdatedIndyItem {
             id: item.id,
-            category: Self::encrypt_merged(
-                item.typ.as_slice(),
-                &key.category_key,
-                Some(&key.item_hmac_key),
-            )?
-            .to_vec(),
-            name: Self::encrypt_merged(
-                item.name.as_slice(),
-                &key.name_key,
-                Some(&key.item_hmac_key),
-            )?,
-            value: Self::encrypt_value(
-                item.typ,
-                item.name,
-                item.value.unwrap(),
-                &key.item_hmac_key,
-            )?,
-            tags,
+            category: key.encrypt_entry_category(item.typ.into())?,
+            name: key.encrypt_entry_name(item.name.into())?,
+            value,
+            tags: key.encrypt_entry_tags(item.tags)?,
         };
 
         Ok(updated_indy_item)
