764f04f38c017beca00c71b9cfb496d875804d4d
KolbyRKunz <kolbykunz@yahoo.com>
2023-06-06 12:19:45 -0600

    Updated Maven wrapper version
    Signed-off-by: KolbyRKunz <kolbykunz@yahoo.com>

--- wrappers/kotlin/build.gradle.kts
+++ wrappers/kotlin/build.gradle.kts
@@ -15,7 +15,7 @@ repositories {
 ext["githubUsername"] = null
 ext["githubToken"] = null
 ext["askarVersion"] = "0.2.9-dev.3"
-ext["wrapperVersion"] = "2"
+ext["wrapperVersion"] = "2.1"
 
 val secretPropsFile = project.rootProject.file("local.properties")
 if(secretPropsFile.exists()) {
