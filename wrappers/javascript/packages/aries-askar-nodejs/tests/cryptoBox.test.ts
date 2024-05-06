89f0950adf2ec58dd97b3db4cd97871eda0ed100
Berend Sliedrecht <sliedrecht@berend.io>
2024-05-06 12:32:15 +0200

    chore(js): use pnpm as a package manager instead of yarn
    Signed-off-by: Berend Sliedrecht <sliedrecht@berend.io>

---
+++ wrappers/javascript/packages/aries-askar-nodejs/tests/cryptoBox.test.ts
@@ -0,0 +1,18 @@
+import { CryptoBox, Key, KeyAlgs } from '@hyperledger/aries-askar-shared'
+
+import { setup } from './utils/initialize'
+
+describe('CryptoBox', () => {
+  beforeAll(setup)
+
+  test('seal', () => {
+    const x25519Key = Key.generate(KeyAlgs.X25519)
+
+    const message = Uint8Array.from(Buffer.from('foobar'))
+    const sealed = CryptoBox.seal({ recipientKey: x25519Key, message })
+
+    const opened = CryptoBox.sealOpen({ recipientKey: x25519Key, ciphertext: sealed })
+    expect(opened).toStrictEqual(message)
+    x25519Key.handle.free()
+  })
+})
---
+++ wrappers/javascript/packages/aries-askar-shared/src/crypto/handles.ts
@@ -0,0 +1,157 @@
+import { ariesAskar } from '../ariesAskar'
+import { AriesAskarError } from '../error'
+
+type ArcHandleType = Uint8Array | string | null
+
+export class ArcHandle {
+  public handle: Uint8Array | string
+
+  public constructor(handle: Uint8Array | string) {
+    if (handle === '0') {
+      throw AriesAskarError.customError({
+        message: 'Invalid handle. This means that the function call succeeded but none was found.',
+      })
+    }
+    this.handle = handle
+  }
+
+  public static fromHandle(handle: ArcHandleType) {
+    return fromPointerHandle(this, handle)
+  }
+}
+
+export class StoreHandle {
+  public handle: number
+
+  public constructor(handle: number) {
+    this.handle = handle
+  }
+
+  public async close() {
+    await ariesAskar.storeClose({ storeHandle: this })
+  }
+
+  public static fromHandle(handle: number | null) {
+    return fromSequenceHandle(this, handle)
+  }
+}
+
+export class ScanHandle {
+  public handle: number
+
+  public constructor(handle: number) {
+    this.handle = handle
+  }
+
+  public free() {
+    ariesAskar.scanFree({ scanHandle: this })
+  }
+
+  public static fromHandle(handle: number | null) {
+    return fromSequenceHandle(this, handle)
+  }
+}
+
+export class SessionHandle {
+  public handle: number
+
+  public constructor(handle: number) {
+    this.handle = handle
+  }
+
+  public async close(commit: boolean) {
+    await ariesAskar.sessionClose({ commit, sessionHandle: this })
+  }
+
+  public static fromHandle(handle: number | null) {
+    return fromSequenceHandle(this, handle)
+  }
+}
+
+export class EntryListHandle extends ArcHandle {
+  public getCategory(index: number) {
+    return ariesAskar.entryListGetCategory({ index, entryListHandle: this })
+  }
+
+  public getName(index: number) {
+    return ariesAskar.entryListGetName({ index, entryListHandle: this })
+  }
+
+  public getValue(index: number) {
+    return ariesAskar.entryListGetValue({ index, entryListHandle: this })
+  }
+
+  public getTags(index: number) {
+    return ariesAskar.entryListGetTags({ index, entryListHandle: this })
+  }
+
+  public free() {
+    ariesAskar.entryListFree({ entryListHandle: this })
+  }
+
+  public static fromHandle(handle: ArcHandleType) {
+    return fromPointerHandle(this, handle)
+  }
+}
+
+export class KeyEntryListHandle extends ArcHandle {
+  public getAlgorithm(index: number) {
+    return ariesAskar.keyEntryListGetAlgorithm({ index, keyEntryListHandle: this })
+  }
+
+  public getName(index: number) {
+    return ariesAskar.keyEntryListGetName({ index, keyEntryListHandle: this })
+  }
+
+  public getTags(index: number) {
+    return ariesAskar.keyEntryListGetTags({ index, keyEntryListHandle: this })
+  }
+
+  public getMetadata(index: number) {
+    return ariesAskar.keyEntryListGetMetadata({ index, keyEntryListHandle: this })
+  }
+
+  public loadKey(index: number) {
+    return ariesAskar.keyEntryListLoadLocal({ index, keyEntryListHandle: this })
+  }
+
+  public free() {
+    ariesAskar.keyEntryListFree({ keyEntryListHandle: this })
+  }
+
+  public static fromHandle(handle: ArcHandleType) {
+    return fromPointerHandle(this, handle)
+  }
+}
+
+export class LocalKeyHandle extends ArcHandle {
+  public free() {
+    ariesAskar.keyFree({ localKeyHandle: this })
+  }
+
+  public static fromHandle(handle: ArcHandleType) {
+    return fromPointerHandle(this, handle)
+  }
+}
+
+/**
+ * Instantiate an handle class based on a received handle. If the handle has a value
+ * of null, the handle class won't be instantiated but rather null will be returned.
+ */
+function fromPointerHandle<HC extends typeof ArcHandle, H extends ArcHandleType>(
+  HandleClass: HC,
+  handle: H,
+): H extends null ? null : InstanceType<HC> {
+  return (handle ? (new HandleClass(handle) as InstanceType<HC>) : null) as H extends null ? null : InstanceType<HC>
+}
+
+function fromSequenceHandle<
+  HC extends typeof StoreHandle | typeof ScanHandle | typeof SessionHandle,
+  H extends number | null,
+>(HandleClass: HC, handle: H): InstanceType<HC> {
+  if (handle === null) {
+    throw AriesAskarError.customError({ message: 'Invalid handle' })
+  }
+
+  return new HandleClass(handle) as InstanceType<HC>
+}
