678bbfc717fc2c2c4442104ff4938fcabddb9d3f
Berend Sliedrecht <sliedrecht@berend.io>
2024-05-08 14:49:28 +0200

    chore(js): migrate to pnpm
    Signed-off-by: Berend Sliedrecht <sliedrecht@berend.io>

--- wrappers/javascript/packages/aries-askar-react-native-example/src/App.tsx
+++ wrappers/javascript/packages/aries-askar-react-native-example/src/App.tsx
@@ -1,4 +1,4 @@
-import { Key, KeyAlgs, KeyBackend, ariesAskar } from '@hyperledger/aries-askar-react-native'
+import { ariesAskar } from '@hyperledger/aries-askar-react-native'
 import { StyleSheet, Text, View } from 'react-native'
 
 const styles = StyleSheet.create({
@@ -10,13 +10,8 @@ const styles = StyleSheet.create({
   },
 })
 
-export const App = () => {
-  const key = Key.generate(KeyAlgs.EcSecp256r1, KeyBackend.SecureElement)
-
-  return (
-    <View style={styles.container}>
-      <Text>{ariesAskar.version()}</Text>
-      <Text>Key: {key.publicBytes.join(',')}</Text>
-    </View>
-  )
-}
+export const App = () => (
+  <View style={styles.container}>
+    <Text>{ariesAskar.version()}</Text>
+  </View>
+)
