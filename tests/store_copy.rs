6601138844716edb42e62d855c5768cc7eb753bb
jamshale <jamiehalebc@gmail.com>
2024-08-21 10:05:45 -0700

    Fix test
    Signed-off-by: jamshale <jamiehalebc@gmail.com>

--- tests/store_copy.rs
+++ tests/store_copy.rs
@@ -51,6 +51,7 @@ fn store_copy() {
                 StoreKeyMethod::RawKey,
                 pass_key_copy,
                 true,
+                ""
             )
             .await
             .expect("Error copying store");
